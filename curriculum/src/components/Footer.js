31034db1b32cb1b1ddbb8148bebf4e2f51fce93d
Helio Souza <heliorr7@gmail.com>
2022-06-14 16:38:56 -0300

    Estilização

--- curriculum/src/components/Footer.js
+++ curriculum/src/components/Footer.js
@@ -1,12 +1,11 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 
 class Footer extends React.Component {
     render() {
         return (
             <div className="footer">
                 <h3>Desenvolvedores</h3>
-                <Link><p>Helio Souza e Patrick Zumach</p></Link>
+                <p>Helio Souza e Patrick Zumach</p>
             </div>
         );
     }
